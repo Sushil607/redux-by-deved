a0f8941f1830e0b164b286d514655de6abdfc399
Sushil607 <bodadesushil@gmail.com>
2023-02-03 22:56:10 +0530

    basic redux boilerplate

--- src/index.js
+++ src/index.js
@@ -1,17 +1,51 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import { createStore } from "redux";
+
+// ACTION ->  describe what are you gonna do eg. increment, decrement
+// simple function that returns an object
+const increment = () => {
+  return {
+    type: "INCREMENT",
+  };
+};
+
+const decrement = () => {
+  return {
+    type: "DECREMENT",
+  };
+};
+
+// REDUCER -> describes how the ACTIONS transform state into next state
+// simple function takes initial state and action parameter
+const counter = (state = 0, action) => {
+  switch (action.type) {
+    case "INCREMENT":
+      return state + 1;
+    case "DECREMENT":
+      return state - 1;
+  }
+};
+
+// STORE -> globalized state, all the data for application
+let store = createStore(counter);
+
+// Display in the console
+store.subscribe(() => console.log(store.getState()));
+
+// DISPATCH -> send the action to reducer
+// execute the action
+store.dispatch(increment());
+store.dispatch(decrement());
+store.dispatch(decrement());
+store.dispatch(increment());
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
